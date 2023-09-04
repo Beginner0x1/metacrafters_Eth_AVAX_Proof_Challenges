677b4da10830e97c5d4dc9a27671f0a059076762
Beginner0x1 <138056577+Beginner0x1@users.noreply.github.com>
2023-09-04 03:29:36 -0400

    Update Module_1_Challenge_3.sol

--- Module_1_Challenge_3.sol
+++ Module_1_Challenge_3.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MessageContextExample {
+    address public sender;
+    uint256 public value;
+    uint256 public timestamp;
+
+    constructor() {
+        // Initialize contract state with message context information
+        sender = msg.sender;       // The sender's address
+        value = msg.value;         // Amount of Ether sent with the transaction
+        timestamp = block.timestamp; // Timestamp of the current block
+    }
+
+    function getMessageContext() public view returns (address, uint256, uint256) {
+        // Return message context information as a tuple
+        return (sender, value, timestamp);
+    }
+}
 
