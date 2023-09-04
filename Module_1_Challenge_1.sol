82d10d5f5117b17a4a930f9e8487764845c8b99c
Beginner0x1 <138056577+Beginner0x1@users.noreply.github.com>
2023-09-04 03:20:08 -0400

    Update Module_1_Challenge_1.sol

--- Module_1_Challenge_1.sol
+++ Module_1_Challenge_1.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract AdminOnlyContract {
+    address public admin;
+
+    constructor() {
+        admin = msg.sender; // The creator of the contract is the initial admin
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+        _;
+    }
+
+    function changeAdmin(address newAdmin) public onlyAdmin {
+        admin = newAdmin;
+    }
+
+    function adminFunction() public onlyAdmin {
+        // This function can only be called by the admin
+        // Add your logic here
+    }
+}
 
